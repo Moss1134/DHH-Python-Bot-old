1b4f4741369846f253fe7e693287b1f9b975b39a
Moss Marcia <136703437+Moss1134@users.noreply.github.com>
2023-07-24 03:20:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
 discord_application_token = "[INSERT YOUR DISCORD BOT TOKEN HERE]"
+guild_veify_channel_id = # Enter the int id for the channel for verifying users
+guild_veified_role_id = # Enter the int id for the 'verified' role, or whatever role you are using to distingish post verified users
+guild_global_role_id = # Enter the int id for the @everyone role
