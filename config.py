35741a53a8372d97bf7dcc1b9909995958e1619f
Moss Marcia <136703437+Moss1134@users.noreply.github.com>
2023-07-28 01:35:09 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
-discord_application_token = "[INSERT YOUR DISCORD BOT TOKEN HERE]"
-guild_veify_channel_id = # Enter the int id for the channel for verifying users
-guild_veified_role_id = # Enter the int id for the 'verified' role, or whatever role you are using to distingish post verified users
-guild_global_role_id = # Enter the int id for the @everyone role
-guild_room_category_id = # Enter the id for the catergory you want your chatrooms to appear under
-verify_message_id = # Enter the id for the verify message
+discord_application_token = ""
+guild_veify_channel_id = 
+guild_veified_role_id = 
+guild_global_role_id = 
+guild_room_category_id = 
+verify_message_id = 
+verified_role_id = 
+reaction_role_age_message_id = 
+age_roles_id = []
+reaction_role_dm_status_message_id = 
+dm_status_roles_id = []
